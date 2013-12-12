04de2812d031d1ce858352f4c49ab25c70646caa
dtynn <dtynn@163.com>
2013-12-12 18:10:44 +0800

    qBucket

--- watcher.py
+++ watcher.py
@@ -1,14 +1,30 @@
 #coding=utf-8
 import pyinotify
-from pyinotify import WatchManager, Notifier, ProcessEvent, ExcludeFilter
+from pyinotify import WatchManager, Notifier, ProcessEvent, ExcludeFilter, AsyncNotifier
 from optparse import OptionParser
+from multiprocessing import Pool, cpu_count
+
+
+def blocker(fpath):
+    import time
+    time.sleep(2)
+    print fpath
+    return
 
 
 class processHandler(ProcessEvent):
+    def my_init(self):
+        processNum = cpu_count()
+        self.pool = Pool(processes=processNum)
+        return
+
     def process_IN_CLOSE_WRITE(self, event):
-        if event.name == '4913' or event.name.endswith('.swp') or event.name.endswith('.swx'):
-            return
+        #if event.name == '4913' or event.name.endswith('.swp') or event.name.endswith('.swx'):
+        #    return
         print event
+        #job = ppserver.submit(blocker, (event.name,))
+        #job()
+        self.pool.apply_async(blocker, (event.name,))
         return
 
     def process_IN_CLOSE_NOWRITE(self, event):
@@ -16,14 +32,14 @@ class processHandler(ProcessEvent):
         return
 
     def process_IN_DELETE(self, event):
-        if event.name.endswith('.swp') or event.name.endswith('.swx'):
-            return
+        #if event.name.endswith('.swp') or event.name.endswith('.swx'):
+        #    return
         print event
         return
 
     def process_IN_MODIFY(self, event):
-        if event.name.endswith('.swp') or event.name.endswith('.swx'):
-            return
+        #if event.name.endswith('.swp') or event.name.endswith('.swx'):
+        #    return
         print event
         return
 
@@ -50,7 +66,7 @@ def main():
         | pyinotify.IN_MODIFY | pyinotify.IN_MOVED_TO | pyinotify.IN_CLOSE_NOWRITE
     excl_list = ['^.*/m3u8$', ]
     excl = ExcludeFilter(excl_list)
-    wadd = wm.add_watch('/home/dtynn/work/test/inotify', mask, rec=True, exclude_filter=excl)
+    wadd = wm.add_watch('/home/dtynn/work/test/qrsync/sync_data', mask, rec=True, exclude_filter=excl)
     notifier = Notifier(wm, processHandler())
     while True:
         try:
